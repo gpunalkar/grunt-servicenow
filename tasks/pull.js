2319724b409b5a98ea733b9ab854b6941c06ca31
Arthur Oliveira <arthur.oliveira@servicenow.com>
2016-02-29 21:42:49 -0800

    Implemented Require config so it doesnt run commands without sn-config .. Also added this file to gitignore

--- tasks/pull.js
+++ tasks/pull.js
@@ -1,7 +1,13 @@
 'use strict';
+var require_config = require("../helper/config_validator");
+
 
 module.exports = function (grunt) {
-    grunt.registerTask('pull', 'Pull command.', function() {
-        grunt.log.writeln("Running pull command");
+    grunt.registerTask('pull', 'Pull command.', function () {
+        var done = this.async();
+        require_config().then(function (config) {
+            grunt.log.writeln("Config file loaded");
+            done();
+        })
     });
 };
