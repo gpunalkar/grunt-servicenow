cf3e09f50bd6ca335374385d90279000c31322a2
Arthur Oliveira <arthur.oliveira@servicenow.com>
2016-03-01 11:58:59 -0800

    Added some comments and TODOS

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,67 +5,82 @@ var path = require('path');
 var inquirer = require('inquirer');
 
 module.exports = function (grunt) {
-	require('load-grunt-tasks')(grunt);
+    require('load-grunt-tasks')(grunt);
 
-	var snConfig = grunt.file.readJSON('.sn-config.json');
-//    var watchObj = {};
-//
-//    for (key in snConfig.folders) {
-//        watchObj[key] = {
-//            files:['dist/'+key+'/*'],
-//            tasks: ['push:'+key+':<%= filechanged %>']
-//        };
-//    }
-	
-	var destination = path.join(process.cwd(), "dist");
-	if(!fs.existsSync(destination)){
-		fs.mkdirSync(destination);
-	}
-	
-	var questions =
-	[{
-		type: "checkbox",
-		config: "tables",
-		message: "What table(s) do you want to pull?",
-		choices: Object.keys(snConfig.folders)
-	}, {
-		type: "confirm",
-		config: "existing_files",
-		message: "Do you want to pull the existing files?",
-		default: false
-	}, {
-		type: "input",
-		config: "specific",
-		message: "What filename do you want to pull?",
-		when: function (answers) {
-			return !answers.existing_files;
-		}
-	}];
-	
+    var snConfig = grunt.file.readJSON('.sn-config.json');
+
+    var watchObj = {};
+    for (key in snConfig.folders) {
+        watchObj[key] = {
+            files: ['dist/' + key + '/*'],
+            tasks: ['push:' + key + ':<%= filechanged %>']
+        };
+    }
+
+    // TODO This should be moved to a task specific file, maybe a helper that runs before every servicenow task
+    var destination = path.join(process.cwd(), "dist");
+    if (!fs.existsSync(destination)) {
+        fs.mkdirSync(destination);
+    }
+
+    // TODO This should be moved to the task specific file. e.g.  ./tasks/pull.js
+    var questions = [
+        {
+            type: "checkbox",
+            config: "tables",
+            message: "What table(s) do you want to pull?",
+            choices: Object.keys(snConfig.folders)
+        }, {
+            type: "confirm",
+            config: "existing_files",
+            message: "Do you want to pull the existing files?",
+            default: false
+        }, {
+            type: "input",
+            config: "specific",
+            message: "What filename do you want to pull?",
+            when: function (answers) {
+                return !answers.existing_files;
+            }
+        }
+    ];
+
     grunt.initConfig({
-//        pkg: grunt.file.readJSON('package.json'),
-//        pull: snConfig.folders,
-//        push: snConfig.folders,
-//        watchAndPush: watchObj,
-		prompt : {
-			pull : {
-				options : {
-					questions : questions
-				}
-			}
-		}
-	
-    });
-	grunt.registerTask("yo",['prompt:pull','pullSN']);
-//    grunt.event.on('watch', function (action, filepath) {
-//        grunt.config(['filechanged'], filepath);
-//    });
+        //This allows us to refer to the values of properties within our package.json file. e.g. <%= pkg.name %>
+        pkg: grunt.file.readJSON('package.json'),
+        //This allows us to refer to the values of properties within our .sn-config.json file. e.g. <%= pkg.project_prefix %>
+        sn_config: snConfig,
+        pull: snConfig.folders,
+        push: snConfig.folders,
+        watchAndPush: watchObj,
+        prompt: {
+            pull: {
+                options: {
+                    questions: questions
+                }
+            }
+        }
 
-//    grunt.loadNpmTasks('grunt-servicenow');
-//    grunt.loadNpmTasks('grunt-contrib-watch');
-//    grunt.task.renameTask("watch", "watchAndPush");
-//	grunt.registerTask("pullSN",[]);
-	grunt.task.loadTasks('./tasks');
-	
+    });
+
+    // TODO This should be removed if we don't want to offer watchAndPush functionality (filesync alike)
+    grunt.event.on('watch', function (action, filepath) {
+        grunt.config(['filechanged'], filepath);
+    });
+
+
+    grunt.loadNpmTasks('grunt-servicenow');
+    //    OR (TBD)
+    // grunt.task.loadTasks('./tasks');
+
+
+    grunt.registerTask("yo", ['prompt:pull', 'pullSN']);
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.task.renameTask("watch", "watchAndPush");
+
+    //	TODO Tasks should be moved to ./tasks/
+    //	Make sure to add new tasks to /tasks/index.js
+    //	grunt.registerTask("pullSN",[]);
+
 
 };
