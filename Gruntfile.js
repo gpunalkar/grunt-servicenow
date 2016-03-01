e7c792d85022123d1ba6fcc6ea254d76557c9032
AJ Siegel <aj.siegel@servicenow.com>
2016-03-01 13:50:21 -0500

    add grunt-prompt and setup 3 questions for pull requests

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,8 @@ var require_config = require("./helper/config_validator");
 
 var fs = require('fs');
 var path = require('path');
+var inquirer = require('inquirer');
+
 module.exports = function (grunt) {
 	require('load-grunt-tasks')(grunt);
 
@@ -14,19 +16,47 @@ module.exports = function (grunt) {
 //            tasks: ['push:'+key+':<%= filechanged %>']
 //        };
 //    }
+	
 	var destination = path.join(process.cwd(), "dist");
 	if(!fs.existsSync(destination)){
 		fs.mkdirSync(destination);
 	}
+	
+	var questions =
+	[{
+		type: "checkbox",
+		config: "tables",
+		message: "What table(s) do you want to pull?",
+		choices: Object.keys(snConfig.folders)
+	}, {
+		type: "confirm",
+		config: "existing_files",
+		message: "Do you want to pull the existing files?",
+		default: false
+	}, {
+		type: "input",
+		config: "specific",
+		message: "What filename do you want to pull?",
+		when: function (answers) {
+			return !answers.existing_files;
+		}
+	}];
+	
     grunt.initConfig({
 //        pkg: grunt.file.readJSON('package.json'),
 //        pull: snConfig.folders,
 //        push: snConfig.folders,
 //        watchAndPush: watchObj,
-			
+		prompt : {
+			pull : {
+				options : {
+					questions : questions
+				}
+			}
+		}
 	
     });
-
+	grunt.registerTask("yo",['prompt:pull','pullSN']);
 //    grunt.event.on('watch', function (action, filepath) {
 //        grunt.config(['filechanged'], filepath);
 //    });
