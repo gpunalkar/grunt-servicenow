fad8866fd45ddffea41ab5e27916c4b05766b685
Arthur Oliveira <arthur.oliveira@servicenow.com>
2016-02-29 23:27:31 -0800

    Implemented watch and pass the changed file to action. E.g. to be used on push on change if desired

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,9 +1,25 @@
-var express = require('express');
-
 module.exports = function (grunt) {
+    var snConfig = grunt.file.readJSON('.sn-config.json');
+
+    grunt.config(['pull', 'filechanged'], "");
+
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
+        pull: snConfig.folders,
+        watch: {
+            ui_scripts: {
+                files: ['dist/ui_scripts/*'],
+                tasks: ['pull:ui_script:<%= pull.filechanged %>']
+            }
+        }
     });
+    grunt.event.on('watch', function (action, filepath) {
+        grunt.config(['pull', 'filechanged'], filepath);
+        //grunt.log.writeln("Watch called");
+    });
+
+
     grunt.loadNpmTasks('grunt-servicenow');
+    grunt.loadNpmTasks('grunt-contrib-watch');
 
 };
--- tasks/pull.js
+++ tasks/pull.js
@@ -3,11 +3,12 @@ var require_config = require("../helper/config_validator");
 
 
 module.exports = function (grunt) {
-    grunt.registerTask('pull', 'Pull command.', function () {
-        var done = this.async();
-        require_config().then(function (config) {
-            grunt.log.writeln("Config file loaded");
-            done();
-        })
+    grunt.registerTask('pull', 'Pull command.', function (arg1, arg2) {
+        grunt.log.writeln("Pulled", arg1, arg2);
+        //var done = this.async();
+        //require_config().then(function (config) {
+        //    grunt.log.writeln("Config file loaded", arg1,arg2);
+        //    done();
+        //})
     });
 };
