a57d926dbefcae46d09d625053456094f1bf29ac
AJ Siegel <aj.siegel@servicenow.com>
2016-03-02 18:37:22 -0500

    creating hash for all record scenarios

--- tasks/pull.js
+++ tasks/pull.js
@@ -69,9 +69,11 @@ module.exports = function (grunt) {
 						else{
 							for(var i = 0; i < obj.result.length; i++){
 								(function(){
-									var content = obj.result[i][config.folders[folderName].field];
+									result = obj.result[i];
+									
+									var content = result[config.folders[folderName].field];
 							
-									var filename = obj.result[i][config.folders[folderName].key];
+									var filename = result[config.folders[folderName].key];
 
 									if ('extension' in config.folders[folderName]) {
 										filename = filename + "." + config.folders[folderName].extension;
@@ -80,15 +82,24 @@ module.exports = function (grunt) {
 									var file_path = path.join(folder_path, filename);
 									
 									// instantiate file_record and create hash
-									fs.writeFile(file_path, content, function (err) {
-										if (err){
-											console.error("Error writing new file", err)
-										}
-										else{
-											console.log("Creating file " + file_path);
-										}
+									var fileRecord = new FileRecord(config, file_path);
 
-										done();
+									fileRecord.updateMeta({
+										sys_id : result.sys_id,
+										sys_updated_on : result.sys_updated_on,
+										sys_updated_by : result.sys_updated_by
+									});
+
+									fileRecord.saveHash(content, function(saved){
+										fs.writeFile(file_path, content, function (err) {
+											if (err){
+												console.error("Error writing new file", err)
+											}
+											else{
+												console.log("Creating file " + file_path);
+											}
+
+										});
 									});
 								})();
 							}
@@ -96,48 +107,7 @@ module.exports = function (grunt) {
 					});
 				});
 				
-//                var query = "name" + "STARTSWITH" + 'solution';
-//
-//
-//                var folder_path = path.join(destination, folderName);
-//
-//
-//                if (!fs.existsSync(folder_path)) {
-//                    fs.mkdirSync(folder_path);
-//                }
-//
-//                snHelper.getRecords(config.folders[folderName].table, query).on("complete", function (data, response) {
-//                    if (data.code === 'ECONNREFUSED') {
-//                        console.log(data);
-//                    }
-//                    else {
-//                        for (var i = 0; i < data.result.length; i++) {
-//                            var content = data.result[i][config.folders[folderName].field];
-//                            var filename = data.result[i].name;
-//
-//                            if ('extension' in config.folders[folderName]) {
-//                                filename = filename + "." + config.folders[folderName].extension;
-//                            }
-//                            var file_path = path.join(folder_path, filename);
-//                            fs.writeFile(file_path, content, function (err) {
-//
-//                                if (err) {
-//                                    console.error("ERR", err);
-//                                }
-//                                else {
-//                                    console.log("Touching file " + file_path);
-//                                    // why is this showing only the second files name twice
-//
-//                                }
-//                                if (i === data.result.length) {
-//                                    done();
-//                                }
-//                            });
-//                        }
-//                    }
-//                    done();
-//
-//                });
+
             });
         });
     });
