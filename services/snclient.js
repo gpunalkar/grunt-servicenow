e2744c79ea2471a444e2723d36233fea8fa0a9ee
AJ Siegel <aj.siegel@servicenow.com>
2016-03-01 23:54:16 -0500

    updated sn client to use restler but follow better patterns from restify model. got the request to return actual records, added url [npm] to devDependencies

--- services/snclient.js
+++ services/snclient.js
@@ -1,18 +1,31 @@
 "use strict";
+var http = require('http');
 var restler = require('restler');
-var snClient = restler.service(
+var url = require('url');
+var util = require('util');
+
+module.exports = restler.service(
 	function(config){
-		this.config = config;
+
+		var auth = new Buffer(config.auth,"base64").toString(),
+			parts = auth.split(":"),
+			username = parts[0],
+			password = parts[1],
+			clientOptions = {
+				baseURL : "https://" + config.host
+			};
 		
-		var auth = new Buffer(config.auth,"base64").toString().split(":");
-		this.defaults.username = auth[0];
-		this.defaults.password = auth[1];
-		
-		this.defaults.headers = {
+		var headers = {
 			'Content-Type': 'application/json',
 			'Accepts': 'application/json'
 		};
-		this.baseURL = "https://" + config.host;
+		
+		this.baseURL = clientOptions.baseURL;
+		
+		this.defaults.username = username;
+		this.defaults.password = password;
+		this.defaults.headers = headers;
+		
 		
 		
 	},
@@ -20,15 +33,126 @@ var snClient = restler.service(
 		
 	},
 	{
-		getRecords : function(table,query){
-			return this.get(this.baseURL + "/api/now/table/" + table + '?sysparm_query=' + query);
-		},
-		postRecord : function(table,data){
-			return this.post(this.baseURL + "/table/" + table,{
-				data : JSON.stringify(data)
-			});
-		},
+//		getRecords : function(table,query){
+////			console.log(this.baseURL + "/api/now/table/" + table + "?sysparm_query=" + query);
+//			return this.get(this.baseURL + "/api/now/table/" + table + "?sysparm_query=" + query);
+//		}
+		table : function table(tableName) {
+			var client = this;
+			
+			function validateResponse(result, res) {
+//				logResponse(result, res);
+				
+				var help = '';
 
+				if(result instanceof Error) {
+					var errorlist = {
+						'ECONNREFUSED': 'Missing interent connection or connection was refused!',
+						'ENOTFOUND': 'No connection available (do we have internet?)',
+						'ETIMEDOUT': 'Connection timed out. Internet down?'
+					};
+
+					help = errorlist[result.code] || "Something failed badly.. internet connectiond own?";
+
+					help += util.format('\ndetails: %j', result);
+					
+					console.warn(help);
+					console.error(result);
+					
+					return new Error(help);
+				}
+				
+				if (res.statusCode !== 200) {
+
+					if (res.statusCode === 401) {
+					  help = 'Check credentials.';
+					} else if (res.statusCode === 302) {
+					  help = 'Verify JSON Web Service plugin is activated.';
+					}
+
+					var message = util.format('%s - %s', res.statusCode, http.STATUS_CODES[res.statusCode]);
+					if (help) {
+					  message += ' - ' + help;
+					}
+					if (result instanceof Error) {
+					  message += util.format('\ndetails: %j', result);
+					}
+					return new Error(message);
+				}
+				
+				if (result instanceof Error) {
+					return result;
+				}
+				if (result.error) {
+					console.error('ERROR found in obj.error : ', result.error);
+					// DP TODO : Investigate: Error: json object is null
+					return new Error(result.error);
+					// this is actually not an error! It's just that the server didn't return anything to us
+					//return null;
+				}
+				if (!result.records) {
+					return new Error(util.format('Response missing "records" key: %j\nCheck server logs.', result));
+				}
+				return null;
+			}
+			
+			function logResponse(result, res){
+				 var resCode = res ? res.statusCode : 'no response';
+				console.error('-------------------------------------------------------');
+				console.error(res);
+				console.error('-------------------------------------------------------');
+			}
+			
+			function send(request){
+				var maxRecords = request.rows || 1;
+				var urlObj = {
+					pathname: '/api/now/table/' + request.table,
+					query : {
+						sysparm_record_count : maxRecords,
+					}
+				};
+				if(request.parmName){
+					urlObj.query['sysparm_' + request.parmName] = request.parmValue;
+				}
+				
+				var path = url.format(urlObj);
+				console.log(client.baseURL + path);
+//				console.debug('snc-client send() path: ' + path);
+				
+				function handleResponse(result, res){
+					var err = validateResponse(result, res, request);
+					request.callback(err, result);
+				}
+				try {
+					if (request.postObj){
+						client.post(path, {data: JSON.stringify(request.postObj)}).on("complete",handleResponse);
+					}
+					else{
+	client.get(path).on("complete",handleResponse);
+						
+					}
+				} catch(err){
+					console.error("Some connection error happened...",err);
+					process.exit(1);
+				}
+			}
+			
+			function getRecords(query, callback) {
+				
+				var parms = {
+					table: tableName,
+					parmName : 'query',
+					parmValue : query,
+					rows : 50,
+					callback : callback
+				};
+				
+				send(parms);
+			}
+			
+			return {
+				getRecords : getRecords
+			};
+			
+		}
 	});
-
-module.exports = snClient;
--- tasks/pull.js
+++ tasks/pull.js
@@ -14,55 +14,56 @@ module.exports = function (grunt) {
             var destination = path.join(process.cwd(), "dist");
             require_folder(destination).then(function () {
 
-                console.log(grunt.config("test"));
-
-
-
                 var snHelper = new ServiceNow(config);
-
-
-                var query = "name" + "STARTSWITH" + 'solution';
-
-
-                var folder_path = path.join(destination, folderName);
-
-
-                if (!fs.existsSync(folder_path)) {
-                    fs.mkdirSync(folder_path);
-                }
-
-                snHelper.getRecords(config.folders[folderName].table, query).on("complete", function (data, response) {
-                    if (data.code === 'ECONNREFUSED') {
-                        console.log(data);
-                    }
-                    else {
-                        for (var i = 0; i < data.result.length; i++) {
-                            var content = data.result[i][config.folders[folderName].field];
-                            var filename = data.result[i].name;
-
-                            if ('extension' in config.folders[folderName]) {
-                                filename = filename + "." + config.folders[folderName].extension;
-                            }
-                            var file_path = path.join(folder_path, filename);
-                            fs.writeFile(file_path, content, function (err) {
-
-                                if (err) {
-                                    console.error("ERR", err);
-                                }
-                                else {
-                                    console.log("Touching file " + file_path);
-                                    // why is this showing only the second files name twice
-
-                                }
-                                if (i === data.result.length) {
-                                    done();
-                                }
-                            });
-                        }
-                    }
-                    done();
-
-                });
+				
+				
+					snHelper.table(config.folders[folderName].table).getRecords("nameSTARTSWITHsolution"
+				,function(err,obj){
+					console.log(obj);
+				});
+				
+//                var query = "name" + "STARTSWITH" + 'solution';
+//
+//
+//                var folder_path = path.join(destination, folderName);
+//
+//
+//                if (!fs.existsSync(folder_path)) {
+//                    fs.mkdirSync(folder_path);
+//                }
+//
+//                snHelper.getRecords(config.folders[folderName].table, query).on("complete", function (data, response) {
+//                    if (data.code === 'ECONNREFUSED') {
+//                        console.log(data);
+//                    }
+//                    else {
+//                        for (var i = 0; i < data.result.length; i++) {
+//                            var content = data.result[i][config.folders[folderName].field];
+//                            var filename = data.result[i].name;
+//
+//                            if ('extension' in config.folders[folderName]) {
+//                                filename = filename + "." + config.folders[folderName].extension;
+//                            }
+//                            var file_path = path.join(folder_path, filename);
+//                            fs.writeFile(file_path, content, function (err) {
+//
+//                                if (err) {
+//                                    console.error("ERR", err);
+//                                }
+//                                else {
+//                                    console.log("Touching file " + file_path);
+//                                    // why is this showing only the second files name twice
+//
+//                                }
+//                                if (i === data.result.length) {
+//                                    done();
+//                                }
+//                            });
+//                        }
+//                    }
+//                    done();
+//
+//                });
             });
         });
     });
