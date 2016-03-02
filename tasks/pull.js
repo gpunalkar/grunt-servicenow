e5a07792e4a35d2f09022cf739d2dd23d752fa35
AJ Siegel <aj.siegel@servicenow.com>
2016-03-02 11:15:37 -0500

    added ability to pull single file, and encapsulated multi file in self invoking function

--- tasks/pull.js
+++ tasks/pull.js
@@ -7,7 +7,7 @@ var fs = require('fs');
 var path = require('path');
 
 module.exports = function (grunt) {
-    grunt.registerTask('pull', 'Pull command.', function (folderName) {
+    grunt.registerTask('pull', 'Pull command.', function (folderName,file_name) {
 
         var done = this.async();
         require_config().then(function (config) {
@@ -15,11 +15,67 @@ module.exports = function (grunt) {
             require_folder(destination).then(function () {
 
                 var snHelper = new ServiceNow(config);
+				var query ="";
 				
-				
-					snHelper.table(config.folders[folderName].table).getRecords("nameSTARTSWITHsolution"
-				,function(err,obj){
-					console.log(obj);
+				if(file_name){
+					query = config.folders[folderName].key + "=" + file_name;	
+				}
+				else{
+					query = config.folders[folderName].key + "STARTSWITHsolution";
+				}
+				snHelper.table(config.folders[folderName].table).getRecords(query,function(err,obj){
+					var folder_path = path.join(destination, folderName);
+					
+					require_folder(folder_path).then(function (){
+						if(obj.result.length === 1){
+							var content = obj.result[0][config.folders[folderName].field];
+							
+							var filename = obj.result[0][config.folders[folderName].key];
+							
+							if ('extension' in config.folders[folderName]) {
+								filename = filename + "." + config.folders[folderName].extension;
+							}
+							
+							var file_path = path.join(folder_path, filename);
+							
+							fs.writeFile(file_path, content, function (err) {
+								if (err){
+									console.error("Error writing new file", err)
+								}
+								else{
+									console.log("Creating file " + file_path);
+								}
+								
+								done();
+							});
+						}
+						else{
+							for(var i = 0; i < obj.result.length; i++){
+								(function(){
+									var content = obj.result[i][config.folders[folderName].field];
+							
+									var filename = obj.result[i][config.folders[folderName].key];
+
+									if ('extension' in config.folders[folderName]) {
+										filename = filename + "." + config.folders[folderName].extension;
+									}
+
+									var file_path = path.join(folder_path, filename);
+
+									fs.writeFile(file_path, content, function (err) {
+										if (err){
+											console.error("Error writing new file", err)
+										}
+										else{
+											console.log("Creating file " + file_path);
+										}
+
+										done();
+									});
+								})();
+							}
+						}
+					});
 				});
 				
 //                var query = "name" + "STARTSWITH" + 'solution';
