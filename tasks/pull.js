0be585c046d72582cebf81541d3e9083f780c532
AJ Siegel <aj.siegel@servicenow.com>
2016-03-03 13:33:54 -0500

    trying to add inquirer

--- tasks/pull.js
+++ tasks/pull.js
@@ -1,6 +1,7 @@
 'use strict';
 var fs = require('fs'),
     path = require('path'),
+	inquirer = require('inquirer'),
     ServiceNow = require('../services/snclient'),
     require_config = require("../helper/config_validator"),
 	fileHelper = require("../helper/file_helper"),
@@ -13,78 +14,108 @@ module.exports = function (grunt) {
 
         var done = this.async();
 		var destination = path.join(process.cwd(), grunt.config('destination'));
-		grunt.option.init([{
+		if(!folder_name && !file_name){
 
-		}])
+			var questions = [
+				{
+					type: "input",
+					name: "host",
+					message: "Enter servicenow instance name (\<instance\>.service-now.com). "
+				},
+				{
+					type: "input",
+					name: "project_prefix",
+					message: "Enter your project prefix (e.g. ProjectName__). "
+				},
+				{
+					type: "input",
+					name: "username",
+					message: "Enter servicenow username. "
+				},
+				{
+					type: "password",
+					message: "Enter your servicenow password. ",
+					name: "password"
+				}
+			];
+			inquirer.prompt(questions, function (answers) {
+				console.log("answers");
+			});
+
+		}
 		syncDataHelper.loadData().then(function (sync_data) {
 			require_config().then(function (config) {
-				var prefix = grunt.config("pull_prefix");
 
-				var hash = HashHelper(sync_data);
+				if(folder_name){
+					var prefix = grunt.config("pull_prefix");
 
-				var snHelper = new ServiceNow(config);
-				var query ="";
+					var hash = HashHelper(sync_data);
 
-				if(file_name){
-					query = config.folders[folder_name].key + "=" + file_name;	
-				}
-				else if(prefix)
-				{
+					var snHelper = new ServiceNow(config);
+					var query ="";
 
-					query = config.folders[folder_name].key + "STARTSWITH" + prefix;
-				}
+					if(file_name){
+						query = config.folders[folder_name].key + "=" + file_name;
+					}
+					else if(prefix)
+					{
 
-				snHelper.table(config.folders[folder_name].table).getRecords(query,function(err,obj){
-					var config_object = config.folders[folder_name];
-					var savePromise = new Promise(function(resolve,reject){
-						var files_to_save = [];
-						for(var i = 0; i < obj.result.length; i++){
-							var result = obj.result[i];
+						query = config.folders[folder_name].key + "STARTSWITH" + prefix;
+					}
 
-							
-							(function(){
-								var file_name = result[config_object.key];
-
-								if(config_object.extension){
-									file_name = file_name + "." + config_object.extension;
-								}
-
-								var dest = path.join(destination, folder_name,file_name);
-
-								var content = result[config_object.field];
-
-								files_to_save[dest] = content;
-								sync_data[dest] = {
-									sys_id: result.sys_id,
-									sys_updated_on: result.sys_updated_on,
-									sys_updated_by: result.sys_updated_by,
-									hash: hash.hashContent(content)
-								};
-
-								if(i === obj.result.length-1){
-									resolve(files_to_save)
-								}
+					snHelper.table(config.folders[folder_name].table).getRecords(query,function(err,obj){
+						var config_object = config.folders[folder_name];
+						var savePromise = new Promise(function(resolve,reject){
+							var files_to_save = [];
+							for(var i = 0; i < obj.result.length; i++){
+								var result = obj.result[i];
 
 
+								(function(){
+									var file_name = result[config_object.key];
 
-							})();
+									if(config_object.extension){
+										file_name = file_name + "." + config_object.extension;
+									}
 
-						}
+									var dest = path.join(destination, folder_name,file_name);
+
+									var content = result[config_object.field];
+
+									files_to_save[dest] = content;
+									sync_data[dest] = {
+										sys_id: result.sys_id,
+										sys_updated_on: result.sys_updated_on,
+										sys_updated_by: result.sys_updated_by,
+										hash: hash.hashContent(content)
+									};
+
+									if(i === obj.result.length-1){
+										resolve(files_to_save)
+									}
+
+
+
+								})();
+
+							}
+						});
+
+						savePromise.then(function(files_to_save){
+
+							fileHelper.saveFiles(files_to_save
+								).then(function(){
+									syncDataHelper.saveData(sync_data);
+									done();
+								},function(err){
+									console.error("Save file failed", err);
+									done();
+								});
+
+						});
 					});
+				}
 
-					savePromise.then(function(files_to_save){
-
-						fileHelper.saveFiles(files_to_save
-							).then(function(){
-								syncDataHelper.saveData(sync_data);
-								done();
-							},function(err){
-								console.error("Save file failed", err);
-								done();
-							});
-
-					});
-				});
 			});
 		});
 
