cdd131aa039d25cd317fd18972a27e628a214a84
Arthur Oliveira <arthur.oliveira@servicenow.com>
2016-03-02 17:27:51 -0800

    Updated helper to return a promise as we need to load the file and read its content

--- helper/hash_helper.js
+++ helper/hash_helper.js
@@ -1,9 +1,5 @@
 module.exports = function (sync_data) {
 
-    var hashContent = function (content) {
-        return crypto.createHash('md5').update(content).digest('hex');
-    };
-
     var saveHash = function (file_path, hash) {
         if (!(file_path in sync_data)) {
             sync_data[file_path] = {}
@@ -12,24 +8,39 @@ module.exports = function (sync_data) {
         //fileHelper.save(sync_data);
     };
 
-    this.compareHashAndSave = function (file_path, new_content, force) {
-        if (typeof force === "undefined")
-            force = false;
-        var hash_comparison,
-            new_hash = hashContent(new_content);
-
-        if (!(file_path in sync_data)) {
-            // Use a logger
-            console.log("Hash doesn't exist, creating one.");
-            hash_comparison = true;
-        } else {
-            hash_comparison = sync_data[file_path].hash == new_hash
-        }
-
-        if (hash_comparison || force) {
-            saveHash(file_path, new_hash);
-        }
-
-        return hash_comparison || force;
+    this.hashContent = function (content) {
+        return crypto.createHash('md5').update(content).digest('hex');
     };
+
+    this.compareHash = function (file_path) {
+        var that = this;
+        return new Promise(function (fulfill, reject) {
+            fs.readFile(config_path, 'utf8', function (err, data) {
+                var hash_comparison;
+
+                if (err) {
+                    console.log(err);
+                } else {
+                    var file_hash = that.hashContent(data);
+                }
+
+                if (!(file_path in sync_data)) {
+                    // Use a logger
+                    console.log("Hash doesn't exist, creating one.");
+                    hash_comparison = true;
+                } else {
+                    hash_comparison = sync_data[file_path].hash == file_hash
+                }
+
+
+                if (hash_comparison) {
+                    fulfill();
+                } else {
+                    reject();
+                }
+            });
+        });
+    };
+
+    return this;
 };
