f55bfd561d4feee05c2eb5e1f4649c6396f86a94
Arthur Oliveira <arthur.oliveira@servicenow.com>
2016-03-07 20:49:25 -0800

    Removed pull like and push like

--- tasks/index.js
+++ tasks/index.js
@@ -3,8 +3,6 @@
 module.exports = function (grunt) {
     require('./init')(grunt);
     require('./pull')(grunt);
-	require('./pullLike')(grunt);
     require('./push')(grunt);
-	require('./pushLike')(grunt);
     require('./runserver')(grunt);
 };
