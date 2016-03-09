6e98532cc26a534876ffa89e51200df4af421d2c
Arthur Oliveira <arthur.oliveira@servicenow.com>
2016-03-08 23:34:59 -0800

    Commented out watch stuff.. doesnt work as the user setup

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,23 +1,21 @@
 module.exports = function (grunt) {
-    var snConfig = grunt.file.readJSON('.sn-config.json');
-
-    var watchObj = {};
-    for (key in snConfig.folders) {
-        watchObj[key] = {
-            files: ['dist/' + key + '/*'],
-            tasks: ['push:' + key + ':<%= filechanged %>']
-        };
-    }
+    
+    //var snConfig = grunt.file.readJSON('.sn-config.json');
+    //var watchObj = {};
+    //for (key in snConfig.folders) {
+    //    watchObj[key] = {
+    //        files: ['dist/' + key + '/*'],
+    //        tasks: ['push:' + key + ':<%= filechanged %>']
+    //    };
+    //}
 
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'), //This allows us to refer to the values of properties within our package.json file. e.g. <%= pkg.name %>
-        sn_config: snConfig, //This allows us to refer to the values of properties within our .sn-config.json file. e.g. <%= pkg.project_prefix %>
         watch: watchObj
     });
-    
+
     grunt.loadNpmTasks('grunt-servicenow');
     grunt.loadNpmTasks('grunt-contrib-watch');
-
     grunt.registerTask('new', ["set_config:new:true", 'push:ui_scripts']);
 
 };
