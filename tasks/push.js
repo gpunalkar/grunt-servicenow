019da30fdac2835e87bc38916c5d264cd25b908f
Arthur Oliveira <arthur.oliveira@servicenow.com>
2016-03-08 22:27:33 -0800

    Implementing force on push

--- tasks/push.js
+++ tasks/push.js
@@ -14,10 +14,8 @@ var fs = require('fs'),
 module.exports = function (grunt) {
     grunt.registerTask('push', 'Push command.', function (folder_name, file_name) {
         var done = this.async();
-        var force = false;
-
         var insert_new_files = grunt.option('new') || grunt.config('new');
-        var force_update = grunt.option('new') || grunt.config('new');
+        var force_update = grunt.option('force') || grunt.config('force');
 
         syncDataHelper.loadData().then(function (sync_data) {
             require_config().then(function (config) {
@@ -99,10 +97,10 @@ module.exports = function (grunt) {
                                         currentPromise = hash.compareHashRemote(record_path, foldername, config);
                                         currentPromise.then(function (sameHash) {
                                             if (sameHash || force_update) {
-                                                console.log('File ' + file_obj.name + ' hasnt changed', record_path);
+                                                //console.log('File ' + file_obj.name + ' hasnt changed', record_path);
                                                 filesToSave[record_path] = file_obj.content;
                                             } else {
-                                                console.log('File ' + file_obj.name + ' changed');
+                                                console.log('File ' + file_obj.name + ' changed, please pull');
                                                 filesChanged.push(file_obj);
                                             }
                                         });
@@ -128,7 +126,9 @@ module.exports = function (grunt) {
                                         servicePromise = updateRecord(fileObj);
                                     }
                                     servicePromise.then(function () {
-                                        sync_data[path]["hash"] = hash.hashContent(fileList[path]);
+                                        if (!insert)
+                                            sync_data[path]["hash"] = hash.hashContent(fileList[path]);
+                                        console.log('Pushed: ' + path);
                                     }, function (e) {
                                         console.log('ERROR', e);
                                     });
